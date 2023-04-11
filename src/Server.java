dff1d4b9bf2f1069e62c0b151547c002080e0a73
FoxnArt <111170011+FoxnArt@users.noreply.github.com>
2023-04-11 09:12:28 +0300

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -5,16 +5,17 @@ import java.net.Socket;
 public class Server {
     public static void main(String[] args) throws IOException {
 
-        ServerSocket serverSocket = new ServerSocket(8085);
+        try (ServerSocket serverSocket = new ServerSocket(8085)) {
+            try (Socket clientSocket = serverSocket.accept()) {
 
-        Socket clientSocket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-        System.out.println("New connection accepted");
-        final String name = in.readLine();
-        System.out.println(name);
-        out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                System.out.println("New connection accepted");
+                final String name = in.readLine();
+                System.out.println(name);
+                out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+            }
+        }
     }
 }
